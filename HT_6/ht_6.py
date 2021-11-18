894c2061e3131c3e09a52c1949dac7befb5167ec
Orel Igor <igor.orel0806@gmail.com>
2021-11-18 18:16:50 +0200

    tasl6.2

--- HT_6/ht_6.py
+++ HT_6/ht_6.py
@@ -1,7 +1,7 @@
 give = int(input('Enter a value'))
-sarch = input('Enter a number to check if its in the value of numbers')
+check = input('Enter a number to check the value of numbers')
 g = 0
-search = sarch.split()
+search = check.split()
 for i in range(0,len(search)):
     if give == int(search[i]):
         g = 1
