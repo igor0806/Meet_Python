ac67801fd43cbfbe1a96e986797d0fe8d4e7dd35
Orel Igor <igor.orel0806@gmail.com>
2021-11-18 19:28:40 +0200

    task6.4

--- HT_6/ht_6.py
+++ HT_6/ht_6.py
@@ -1,16 +1,16 @@
-value_a = int(input("Enter a value a: "))
-value_b = int(input("Enter a value b: "))
-search = input("Enter a number to check if its in the value of numbers")
+value_a = int(input('Enter a value a: '))
+value_b = int(input('Enter a value b: '))
+search = input('Enter a number to check the value of numbers')
 search_list = search.split(',')
 g = []
 
 for i in search_list:
     g.append(int(i))
 if value_a in g:
-    print("True")
+    print('True')
 else:
-    print("False")
+    print('False')
 if value_b in g:
-    print("True")
+    print('True')
 else:
-    print("False")
+    print('False')
