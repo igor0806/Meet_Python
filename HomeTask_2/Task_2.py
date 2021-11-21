19ea1887cebc1a33a1b6324238b2f4097f740d23
Orel Igor <igor.orel0806@gmail.com>
2021-11-22 00:02:17 +0200

    task2.1

--- HomeTask_2/Task_2.py
+++ HomeTask_2/Task_2.py
@@ -1,4 +1,14 @@
-# 2. Написати скрипт, який пройдеться по списку, який складається із кортежів, і замінить для кожного кортежа останнє значення.
+# 2. Написати скрипт, який пройдеться по списку, який складається із кортежів, і замінить для кожного кортежа останнє значення
 
-dict_list = [(10, 20, 40), (40, 50, 60), (70, 80, 90)]
-print([n[:-1] + (100,) for n in dict_list])
+input_list = input("Type value: ")
+List_of_tuples = [(10, 20, 40, 'car'), (40, 50, 'green', 60), (70, 80, 90, 'cool')]
+
+list_number = []
+
+for i in List_of_tuples:
+    new_val = list(i)
+    new_val[-1] = input_list
+    changeList = tuple(new_val)
+    list_number.append(changeList)
+print(list_number)
+
