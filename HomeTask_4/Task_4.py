522a85438c2428f4383d58fe4e2b56e09a7f40a9
Orel Igor <igor.orel0806@gmail.com>
2021-11-29 18:45:20 +0200

    Task4.3

--- HomeTask_4/Task_4.py
+++ HomeTask_4/Task_4.py
@@ -1,13 +1,18 @@
 # 4. Написати функцію < prime_list >, яка прийматиме 2 аргументи - початок і кінець діапазона,
 # і вертатиме список простих чисел всередині цього діапазона.
 
-import sympy
-
 def prime_list(start, end):
-    prime_list_result = [value for value in range(start, end) if sympy.isprime(value)]
-    return prime_list_result
+    list_1 = []
+    for i in range(start, end + 1):
+        for j in range(2,i):
+            if i%j==0:
+                break
+        else:
+            list_1.append(i)
+    return list_1
 
-
-print(prime_list(1,1000))
+first_num = int(input('введите начальное число: \n'))
+second_num = int(input('введите до какого числа получить списое: \n'))
+print(prime_list(first_num, second_num))
 
 
